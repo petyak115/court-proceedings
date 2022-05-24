64268855e585c187c3ef4953034ebacedf112c0c
petyak115 <73501822+petyak115@users.noreply.github.com>
2022-05-24 23:21:38 +0300

    Update patient.sol

--- Solidity Contracts/patient.sol
+++ Solidity Contracts/patient.sol
@@ -44,7 +44,15 @@ contract Patient {
       }
       
       
-         }
+           // modifier to give access only to hospital
+      modifier isOwner() {
+
+         require(msg.sender = owner, "Access is not allowed");
+
+         _;
+
+     }
+      
      
       
      
