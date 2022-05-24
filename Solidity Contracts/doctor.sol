c07b72a48bb1ec960c2225b240d5f21bb8a138e4
petyak115 <73501822+petyak115@users.noreply.github.com>
2022-05-24 22:08:44 +0300

    Update doctor.sol

--- Solidity Contracts/doctor.sol
+++ Solidity Contracts/doctor.sol
@@ -22,7 +22,7 @@ contract Doctor {
      
    
       constructor()  public {
-          owner = 0xcdb13bB268b8050A8a1Be66911d1f682492db4Dd; //Address of Hospital
+          owner = 0xcdb13bb268b8050a8a1be66911d1f682492db4dd; //Address of Hospital
       }             
       
      // modifier to give access only to hospital
